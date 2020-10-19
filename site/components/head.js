5bc8fd5d80ea2feea690933c4e0a1cc537289631
hjoelh <jhollingsmith@gmail.com>
2020-10-19 23:36:22 +0100

    site meta

--- site/components/head.js
+++ site/components/head.js
@@ -3,22 +3,21 @@ import Head from "next/head";
 export default function HeadMeta(){
   return (
   <Head>
-    <title>Lofi Radio Desktop</title>
-    <meta name="title" content="Lofi Radio Desktop" />
-    <meta name="description" content="Enjoy the lofi hip hop radio stream on your Desktop - for Mac & PC"/>
+    <title>Eth Price Discord Bot</title>
+    <meta name="title" content="Eth Price Discord Bot" />
+    <meta name="description" content="Ethereum price bot for Discord"/>
 
     <meta property="og:type" content="website"/>
-    <meta property="og:url" content="https://lofi-radio-desktop.vercel.app"/>
+    <meta property="og:url" content="https://eth-price-discord-bot.vercel.app"/>
     <meta property="og:title" content="Lofi Radio Desktop"/>
-    <meta property="og:description" content="Enjoy the lofi hip hop radio stream on your Desktop - for Mac & PC"/>
-    <meta property="og:image" content="../public/meta/main.jpg"/>
+    <meta property="og:description" content="Ethereum price bot for Discord"/>
+    <meta property="og:image" content="https://i.imgur.com/DF2mVL0.jpg"/>
 
     <meta property="twitter:card" content="summary_large_image"/>
-    <meta property="twitter:url" content="https://lofi-radio-desktop.vercel.app"/>
-    <meta property="twitter:title" content="Lofi Radio Desktop"/>
-    <meta property="twitter:description" content="Enjoy the lofi hip hop radio stream on your Desktop - for Mac & PC"/>
-    <meta property="twitter:image" content="https://i.imgur.com/Lg3PMpn.png" />
+    <meta property="twitter:url" content="https://eth-price-discord-bot.vercel.app"/>
+    <meta property="twitter:title" content="Eth Price Discord Bot"/>
+    <meta property="twitter:description" content="Ethereum price bot for Discord"/>
+    <meta property="twitter:image" content="https://i.imgur.com/DF2mVL0.jpg" />
   </Head>
    );
 }
- 
