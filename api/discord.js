a4aa2a2cf1b43391f07dcc93cd3aeb265eb1e3f2
hjoelh <jhollingsmith@gmail.com>
2020-12-04 13:53:46 +0000

    tidy

--- api/discord.js
+++ api/discord.js
@@ -3,20 +3,14 @@ const fetch = require("node-fetch");
 
 const client = new Discord.Client();
 
-async function AsyncLogin() {
-  await client.login(process.env.TOKEN);
-}
-
 async function getPrice() {
-  let raw = await fetch("https://api.coinbase.com/v2/prices/eth-usd/spot");
-  let json = await raw.json();
-  let eth = await json.data.amount;
-  let ethStr = await eth.toString();
-  return ethStr;
-}
+  const raw = await fetch("https://api.coinbase.com/v2/prices/eth-usd/spot");
+    const json = await raw.json();
+      return json.data.amount
+};
 
 module.exports = async (req, res) => {
-  await AsyncLogin();
+  await client.login(process.env.TOKEN)
   const ethPrice = await getPrice();
   await client.user.setActivity("$" + ethPrice, { type: "WATCHING" });
   res.status(200).send(`great success! - $${ethPrice}`);
