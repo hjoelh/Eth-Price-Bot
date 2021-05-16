d616ec8b1ab121193ca01802dd535bd6e1587812
Joel <68335961+hjoelh@users.noreply.github.com>
2021-05-16 10:15:29 +0100

    updated to es modules

--- bot.js
+++ bot.js
@@ -1,21 +1,23 @@
-const fetch = require("node-fetch");
-const Discord = require('discord.js');
+import fetch from "node-fetch";
+import Discord from "discord.js";
 
 const client = new Discord.Client();
 
 client.login(process.env.TOKEN);
 
 async function getPrice() {
-    let raw = await fetch('https://api.coinbase.com/v2/prices/eth-usd/spot');
-      let json = await raw.json()
-        return json.data.amount
-};
+  const raw = await fetch("https://api.coinbase.com/v2/prices/eth-usd/spot");
+    const { data } = await raw.json();
+      return data.amount;
+}
 
 async function main() {
-  let ethPrice = await getPrice()
-  await client.user.setActivity(`$${ethPrice}`, { type: 'WATCHING' })
-    .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
-    .catch(err => console.log(err));
-};
+  client.user
+    .setActivity(`$${await getPrice()}`, { type: "WATCHING" })
+    .then((presence) =>
+      console.log(`Activity set to ${presence.activities[0].name}`)
+    )
+    .catch((err) => console.log(err));
+}
 
 setInterval(() => main(), 30000);
