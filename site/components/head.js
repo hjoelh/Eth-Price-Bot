51a59e58cc9a57acd027a5373afae4099a3ced52
Joel <68335961+hjoelh@users.noreply.github.com>
2021-06-13 20:30:07 +0100

    meta

--- site/components/head.js
+++ site/components/head.js
@@ -6,18 +6,18 @@ export default function HeadMeta(){
       <title>Eth Price Discord Bot</title>
       <script async src="https://cdn.splitbee.io/sb.js"></script>
       <meta name="title" content="Eth Price Discord Bot" />
-      <meta name="description" content="Ethereum price bot for Discord"/>
+      <meta name="description" content="Ethereum Price Bot for Discord"/>
 
       <meta property="og:type" content="website"/>
       <meta property="og:url" content="https://eth-price-discord-bot.vercel.app"/>
       <meta property="og:title" content="Lofi Radio Desktop"/>
-      <meta property="og:description" content="Ethereum price bot for Discord"/>
+      <meta property="og:description" content="Ethereum Price Bot for Discord"/>
       <meta property="og:image" content="https://i.imgur.com/DF2mVL0.jpg"/>
 
       <meta property="twitter:card" content="summary_large_image"/>
       <meta property="twitter:url" content="https://eth-price-discord-bot.vercel.app"/>
       <meta property="twitter:title" content="Eth Price Discord Bot"/>
-      <meta property="twitter:description" content="Ethereum price bot for Discord"/>
+      <meta property="twitter:description" content="Ethereum Price Bot for Discord"/>
       <meta property="twitter:image" content="https://i.imgur.com/DF2mVL0.jpg" />
   </Head>
    );
