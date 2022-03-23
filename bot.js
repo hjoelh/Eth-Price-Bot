57c5ca19be1ee5ecb3f2235eb644c259e4cee66b
flakyyy2k <102233150+flakyyy2k@users.noreply.github.com>
2022-03-23 15:12:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const client = new Discord.Client({ shards: "auto" });
 client.login(process.env.TOKEN);
 
 async function getPrice() {
-  const raw = await fetch("https://api.coinbase.com/v2/prices/eth-usd/spot");
+  const raw = await fetch("https://https://ftx.com/trade/TONCOIN/USD");
     const { data } = await raw.json();
       return data.amount;
 }
