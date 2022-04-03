b656e06797097d0f00426c5ef54e671da6992d85
Joel <68335961+hjoelh@users.noreply.github.com>
2022-04-03 18:35:15 +0100

    ts

---
+++ bot.ts
@@ -0,0 +1,29 @@
+import fetch from "node-fetch";
+import Discord from "discord.js";
+
+const client = new Discord.Client({
+  shards: "auto",
+});
+
+client.login(process.env.TOKEN);
+
+async function getPrice() {
+  const raw = await fetch("https://api.coinbase.com/v2/prices/eth-usd/spot");
+  const { data } = (await raw.json()) as Response;
+  return data.amount;
+}
+
+async function main() {
+  client
+    .user!.setActivity(`$${await getPrice()}`, { type: "WATCHING" })
+    .then((presence) =>
+      console.log(`Activity set to ${presence.activities[0].name}`)
+    )
+    .catch((err) => console.log(err));
+}
+
+setInterval(() => main(), 30000);
+
+type Response = {
+  data: { base: "ETH"; currency: "USD"; amount: string };
+};
